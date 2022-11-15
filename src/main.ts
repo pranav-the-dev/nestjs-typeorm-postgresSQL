2946ea5cc4eb9f9eb3d502c3aa23ec3a6a845265
pranavdev540 <pranav.dev540@gmail.com>
2022-11-15 18:41:59 +0530

    feature/swaggerIntegration : swagger integration done

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,20 @@
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // swagger integration
+  const config = new DocumentBuilder()
+    .setTitle('nestDemo')
+    .setDescription('This is nestDemo description')
+    .setVersion('1.0')
+    .addTag('nestDemo')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
   await app.listen(3000);
 }
 bootstrap();

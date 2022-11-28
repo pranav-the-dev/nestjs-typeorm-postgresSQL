9466e3ddb4472475ea4bd52f92e517a63c6d043b
Pranav mistry <102344790+pranavdev540@users.noreply.github.com>
2022-11-28 16:08:47 +0530

    📈 IMPROVE: migration improved|

--- typeorm-config.ts
+++ typeorm-config.ts
@@ -13,11 +13,13 @@ export const typeOrmAsyncConfig: TypeOrmModuleAsyncOptions = {
       password: configService.get('DB_PASSWORD'),
       database: configService.get('DB_DATABASE'),
       // entities: entities,
-      entities: [__dirname + '/../**/entities/*.entity{.ts,.js}'],
-      migrations: [__dirname + '/migrations/**/*{.ts,.js}'],
-      seeds: [__dirname + '/seeds/**/*{.ts,.js}'],
+      // IMP == entities file which are stored in the local
+      entities: [__dirname + '../entities/*{.ts,.js}'],
+      migrations: [__dirname + '/migrations/*{.ts,.js}'],
       factories: [__dirname + '/factories/**/*{.ts,.js}'],
+      seeds: [__dirname + '/seeds/**/*{.ts,.js}'],
       cli: {
+        // IMP == here migrationDir is the directory on dist folder where migration files are located after execution of the migration
         migrationsDir: __dirname + 'src/migrations/migrations',
         // seeds: [__dirname + 'src/database/seeds/**/*{.ts,.js}'],
         // factories: [__dirname + 'src/database/factories/**/*{.ts,.js}'],
@@ -25,6 +27,8 @@ export const typeOrmAsyncConfig: TypeOrmModuleAsyncOptions = {
       dropSchema: false,
       synchronize: true,
       logging: true,
+      migrationsTableName: 'migrations_history',
+      migrationsRun: true,
     };
   },
 };
